01187f8861cdb8a969307494402e08d57872732d
neerajp99 <neerajp1999@gmail.com>
2020-03-15 12:13:56 +0530

    Add new methods to single task element

--- client-beautiful/src/components/task.js
+++ client-beautiful/src/components/task.js
@@ -5,12 +5,15 @@ import { Draggable } from "react-beautiful-dnd";
 const Task = props => {
   return (
     <Draggable draggableId={props.task.id} index={props.index}>
-      {provided => (
+      {(provided, snapshot) => (
         <Container
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
         >
+    {/*moving the {} up will make the entire div as the draggable handle*/}
+        {/*<Handle  {...provided.dragHandleProps}/>*/}
           {props.task.content}
         </Container>
       )}
@@ -21,8 +24,17 @@ const Task = props => {
 export default Task;
 
 const Container = styled.div`
-  border: 1px solid #000;
+  border: 0.2px solid #000;
   padding: 8px;
   margin-top: 8px;
-  background-color: #fff;
+  background-color: ${props => (props.isDragging ? "lightblue" : "white")};
+  display: flex;
+`;
+
+const Handle = styled.div `
+    width: 20px;
+    height: 20px;
+    background-color: orange;
+    border-radius: 4px;
+    margin-right: 5px;
 `;
